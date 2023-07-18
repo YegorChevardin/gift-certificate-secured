024a26132065e7f4abfbb0e6c7f29c3c688d4b43
YegorChevardin <egor03052004@gmail.com>
2023-07-18 21:40:52 +0200

    Added cors front-end url

--- src/main/java/ua/com/epam/lab/yegorchevardin/springboot/giftcertificate/web/configurations/WebSecurityConfiguration.java
+++ src/main/java/ua/com/epam/lab/yegorchevardin/springboot/giftcertificate/web/configurations/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package ua.com.epam.lab.yegorchevardin.springboot.giftcertificate.web.configurations;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ua.com.epam.lab.yegorchevardin.springboot.giftcertificate.web.configurations.jwt.JwtRequestFilter;
@@ -26,19 +30,20 @@ import ua.com.epam.lab.yegorchevardin.springboot.giftcertificate.web.constants.A
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfiguration {
+public class WebSecurityConfiguration implements WebMvcConfigurer {
     private final UserDetailsService userDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("Authentication");
-            }
-        };
+    @Value("${frontEnd.url}")
+    private String frontEndUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins(frontEndUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 
     @Bean
